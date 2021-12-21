0bbcd7aa114a69f50a443f22263d33412d3b5bed
MashaPav <masha.pavlotsky@logz.io>
2021-12-21 20:43:36 +0200

    FE Task

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+
+import React from 'react';
+import { useAuth, AdminPortal } from '@frontegg/react'
+import profile from './lucky-cat.jpeg';
+import ReactRoundedImage from "react-rounded-image";
+
+const handleClick = () => {
+  AdminPortal.show();
+};
+
+function App() {
+  const { user, isAuthenticated } = useAuth();
+
+
+  return (
+  <div>
+      {isAuthenticated && (
+        <div className="Main App">
+          <ReactRoundedImage image={profile} roundedSize="0" imageWidth="110" imageHeight="110" margin="3"alt={user.name} />
+          <span style={{padding: '3px'}}>{user.name}</span>
+          <button className="Button" onClick={handleClick}>Settings</button>
+          <p> Select tenant : </p>
+          <select className="custom-select">{user.tenants.map(tenant => <option key={tenant.tenantId}>{tenant.tenantId}</option>)}</select>
+        </div>
+      )}
+  </div>
+  );
+}
+
+export default App;
